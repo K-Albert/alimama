728b5e52e8b6a7e7904080bea60967544da06d7e
tianchang <729376693@qq.com>
2018-04-17 16:05:02 +0800

    增加了EDA 数据可视化

--- my_exploration.py
+++ my_exploration.py
@@ -1,42 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 17 14:17:23 2018
+
+@author: lenovo
+"""
+
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import StratifiedKFold
-from lxyTools.singleModelUtils import singleModel
-from sklearn.metrics import log_loss
-import lightgbm as lgb
-from lxyTools.featureSelect import maximalInformationCoef
-from lxyTools.featureSelect import micCompute
 
-testData = pd.read_table(
-    'data/round1_ijcai_18_test_a_20180301.txt', sep=' ')
-trainData = pd.read_table(
-    'data/round1_ijcai_18_train_20180301.txt', sep=' ')
+trainData = pd.read_csv('data/round1_ijcai_18_train_20180301.txt',sep=" ")
+trainData = trainData.drop_duplicates(['instance_id'])
+trainData = trainData.reset_index(drop=True)
 
-trainData['context_timestamp'] = pd.to_datetime(
-    trainData['context_timestamp'], unit='s')
+testData = pd.read_csv('data/round1_ijcai_18_test_a_20180301.txt',sep=" ")
+def time2cov(time_):
+    '''
+    时间是根据天数推移，所以日期为脱敏，但是时间本身不脱敏
+    :param time_:
+    :return:
+    '''
+    return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time_))
+def splitPredicCategory(s):
+    s=s.split(';')
+    category_list=[]
+    for i in s:
+        category_list.append(i.split(':')[0])
+    category= ';'.join(category_list)
+    return category
+def splitPredicProperty(s):
+    s=s.split(';')
+    property_list=[]
+    a=1
+    for i in s:
+        property_split=i.split(':')
+        if len(property_split)==1:
+            a=1
+        else:
+            property_list.append(property_split[1])
+    property= ','.join(property_list) 
+    return property
+def predictPropertyRight(s):
+    s=s.split(':')
+    s1=s[0].split(';')
+    s2=s[1].split(',')
+    property_num=len(s1)
+    predict_right_num=0
+    for i in s2:
+        if i in s1:
+            predict_right_num=predict_right_num+1
+    return predict_right_num/property_num
+def predictCategoryRight(s):
+    s=s.split(':')
+    s1=s[0].split(';')
+    s2=s[1].split(';')
+    category_num=len(s1)
+    predict_right_num=0
+    for i in s2:
+        if i in s1:
+            predict_right_num=predict_right_num+1
+    return predict_right_num/category_num   
+def predictPropertyRightNum(s):
+    s=s.split(':')
+    s1=s[0].split(';')
+    s2=s[1].split(',')
+    property_num=len(s1)
+    predict_right_num=0
+    for i in s2:
+        if i in s1:
+            predict_right_num=predict_right_num+1
+    return predict_right_num  
+def map_hour(x):
+    if (x>=7)&(x<=12):
+        return 1
+    elif (x>=13)&(x<=18):
+        return 2
+    elif ((x>=19)&(x<=23))|(x==0):
+        return 3
+    else:
+        return 4
+#def map_age(x):
+#    if x    
 
-testData['context_timestamp'] = pd.to_datetime(
-    testData['context_timestamp'], unit='s')
-
-
-trainData.insert(loc=0, column='day', value=-1)
+#%%
+trainData['context_timestamp'] =pd.to_datetime(trainData['context_timestamp'].apply(time2cov))
+testData['context_timestamp'] = pd.to_datetime(testData['context_timestamp'].apply(time2cov))
+trainData.insert(loc=0, column='day', value=trainData.context_timestamp.dt.day)
 trainData.insert(loc=0, column='hour', value=trainData.context_timestamp.dt.hour)
 
-testData.insert(loc=0, column='day', value=-1)
+testData.insert(loc=0, column='day', value=testData.context_timestamp.dt.day)
 testData.insert(loc=0, column='hour', value=testData.context_timestamp.dt.hour)
-
-
-minTime = pd.to_datetime('2018-9-17 16:00:00')
-for i in range(8):
-    trainData.loc[
-        trainData.context_timestamp >= minTime+pd.to_timedelta(str(i)+' days'),
-        'day'] += 1
-    testData.loc[
-        testData.context_timestamp >= minTime+pd.to_timedelta(str(i)+' days'),
-        'day'] += 1
 
 num1 = trainData[trainData.is_trade == 1].is_trade.count()
 num0 = trainData[trainData.is_trade == 0].is_trade.count()
@@ -46,14 +102,14 @@ print('为1的比例: ', num1/(num1+num0))
 print('-----'*5)
 print('train maxTime: ', trainData['context_timestamp'].max())
 print('train minTime: ', trainData['context_timestamp'].min())
-print('test maxTime: ', testData['context_timestamp'].min())
-print('test minTime: ', testData['context_timestamp'].max())
+print('test minTime: ', testData['context_timestamp'].min())
+print('test maxTime: ', testData['context_timestamp'].max())
 
 print('-----'*5)
 print('trainData cat:')
 for col in trainData.columns:
     if 'id' in col:
-        print(col, '\tnum: ', len(trainData[trainData.day==6][col].unique()))
+        print(col, '\tnum: ', len(trainData[trainData.day==24][col].unique()))
 print('-----'*5)
 print('testData cat:')
 for col in testData.columns:
@@ -62,14 +118,13 @@ for col in testData.columns:
 
 print('-----'*5)
 print('rData cat:')
-rData = trainData[trainData.day==6].sample(frac=0.33)
+rData = trainData[trainData.day==24].sample(frac=0.33)
 for col in trainData.columns:
     if 'id' in col:
         print(col, '\tnum: ', len(rData[col].unique()))
 
 print('对比上面的这三个结果， 我们可以推断出测试集是由于最后一天的随机采样得到')
-print('-----'*5)
-
+#%%
 alldata = trainData.append(testData)
 
 plt.figure()
@@ -77,16 +132,11 @@ sns.countplot(alldata.day)
 
 g = sns.FacetGrid(alldata, col="day")
 g.map(sns.distplot, 'hour')
+
 g = sns.FacetGrid(trainData, col="day")
 g.map(sns.barplot, 'hour', 'is_trade')
 
-g = sns.FacetGrid(trainData, col="day")
-g.map(sns.barplot, 'user_gender_id', 'is_trade')
-
-g = sns.FacetGrid(trainData, col="day")
-g.map(sns.barplot, 'user_occupation_id', 'is_trade')
-
-sns.factorplot(x='context_page_id', y='is_trade', col='day', data=trainData)
+sns.factorplot(x='hour', y='is_trade', col='day', data=trainData)
 sns.factorplot(x='context_page_id', y='is_trade', data=trainData)
 
 plt.figure()
@@ -94,6 +144,11 @@ sns.barplot(x='day', y='is_trade', data=trainData)
 
 plt.figure()
 sns.countplot(alldata.hour)
+
+
+plt.figure()
+sns.countplot(trainData[trainData['is_trade']==1].hour)
+
 
 plt.figure(figsize=(10,5))
 sns.barplot(x="hour", y="is_trade", data=trainData);
@@ -152,5 +207,17 @@ plt.figure()
 g = sns.FacetGrid(trainData, col="day")
 g.map(sns.barplot, 'item_cat2_id', 'is_trade')
 
-#for i in trainData.item_cat_id.unique():
-#    print(trainData[trainData.item_cat_id==i].count())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
